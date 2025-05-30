0985f9ec2d515ae2f06b80c1a0a31f189b06cfe5
Ubuntu <ubuntu@ip-172-31-92-12.ec2.internal>
2025-05-30 23:01:17 +0000

    Added boot.asm

---
+++ boot1.asm
@@ -0,0 +1,55 @@
+ORG 0x7c00
+BITS 16
+
+_start:
+	mov ah, 0eh
+	mov al,'M'
+	int 0x10
+	jmp short testing
+testing:
+	mov ah, 0eh
+	mov al,'A'
+	int 0x10
+	jmp short print1
+handle_zero:
+	mov ah, 0eh
+	mov al,'A'
+	int 0x10
+	iret
+
+	;cli
+	;mov ax, 0x7c0
+	;mov ds, ax
+	;mov es, ax
+	;mov ax, 0x00
+	;mov ss, ax
+	;mov sp, 0x7c00
+	;sti
+print1:
+	mov ah, 0eh
+	mov al,'Z'
+	int 0x10
+	mov word[ss:0x00], handle_zero
+	mov word[ss:0x02], 0x7c0
+	int 0
+	;mov si, message
+	;call print
+	jmp $
+print:
+	.loop:
+		lodsb
+		cmp al,1
+		je .done
+		call print_char
+		jmp .loop
+	.done:
+		jmp $
+		ret
+		
+	print_char:
+	mov ah, 0eh
+	int 0x10
+	ret
+message: db 'Hello World!',1
+times 510 - ($ - $$) db 0	
+dw 0xAA55
