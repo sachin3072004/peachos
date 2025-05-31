9a9cb712395441bf381a173566534c5517a3ca1f
Ubuntu <ubuntu@ip-172-31-81-100.ec2.internal>
2025-05-31 13:21:45 +0000

    Bios is reading from the HD

--- boot.asm
+++ boot.asm
@@ -11,8 +11,23 @@ _start:
 	mov ss, ax
 	mov sp, 0x7c00
 	sti
+	mov ah, 2
+	mov al, 1
+	mov ch, 0
+	mov cl, 2
+	mov dh, 0
+	mov bx,buffer	
+	int 0x13
+	jc error 
+	mov si, buffer
+	call print
+	jmp $
 	jmp  print1
 
+error:
+	mov si, error_message
+	call print
+	jmp $
 handle_zero:
 	mov ah, 0eh
 	mov al,'A'
@@ -46,7 +61,7 @@ print1:
 print:
 	.loop:
 		lodsb
-		cmp al,1
+		cmp al,0
 		je .done
 		call print_char
 		jmp .loop
@@ -57,6 +72,8 @@ print:
 	mov ah, 0eh
 	int 0x10
 	ret
-message: db 'Hello World!',1
+error_message: db 'Failed to load sector',0
+message: db 'Hello World!',0
 times 510 - ($ - $$) db 0	
 dw 0xAA55
+buffer:
